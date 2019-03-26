244092bcc6dc547432bab12936978e45fb50ab25
MrCandle <rodrigofvela@gmail.com>
2019-03-26 20:15:39 -0300

    enable https

--- Startup.cs
+++ Startup.cs
@@ -43,7 +43,7 @@ namespace HelloWorld
                 app.UseHsts();
             }
 
-            //app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
 
